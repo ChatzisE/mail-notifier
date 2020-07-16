53f15b8b7c30ea91ad549f8db834823b7ca22830
echatzis <tzisxa1@gmail.com>
2020-07-16 13:27:36 +0300

    add timezone

--- src/app/Services/SendMailService.cs
+++ src/app/Services/SendMailService.cs
@@ -14,7 +14,8 @@ namespace mail_notifier.Services
         private static IConfiguration _config;
         private const string SUBJECT = "Your civil service appointment";
         public SendMailService(IConfiguration config)
-        {;
+        {
+            ;
             _config = config;
         }
         public async Task<Response> CreateAndSendMail(MailInfo info)
@@ -22,14 +23,22 @@ namespace mail_notifier.Services
             string apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
             SendGridClient client = new SendGridClient(apiKey);
             EmailAddress from = new EmailAddress("itp18406@hua.gr", "Easy Apointment");
-            EmailAddress to = new EmailAddress(info.email,null);
-            string htmlContent = @"<strong>"+info.userName +" " + info.userSurname + "</strong>"+
+            EmailAddress to = new EmailAddress(info.email, null);
+            string correctDt = ConvertToAthensTime(info.appointmentDate);
+            string htmlContent = @"<strong>" + info.userName + " " + info.userSurname + "</strong>" +
                                      "<br>Your appointment at  <ins>" + info.appointmentPlace +
-                                     "</ins> has been  succesfully aranged for <ins>" + info.appointmentDate.ToString("MM/dd/yy H:mm")+ "</ins>" +
+                                     "</ins> has been  succesfully aranged for <ins>" + correctDt + "</ins>" +
                                      "<br> <strong>For any futher clarification please feel free to contact us!</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, SUBJECT,null,htmlContent);
+            var msg = MailHelper.CreateSingleEmail(from, to, SUBJECT, null, htmlContent);
             var response = await client.SendEmailAsync(msg);
             return response;
+        }
+        private string ConvertToAthensTime(DateTime input)
+        {
+            TimeZoneInfo est = TimeZoneInfo.FindSystemTimeZoneById("Europe/Athens");
+            DateTime targetTime = TimeZoneInfo.ConvertTime(input, est);
+            Console.WriteLine(targetTime.ToString("MM/dd/yy H:mm"));
+            return targetTime.ToString("MM/dd/yy H:mm");
         }
     }
 }
